9f6f4ea40ca3835724d3fa5e3d55b2190c4b4476
Dušan Reljić <reljicd@gmail.com>
2017-12-28 16:10:32 +0100

    Cleanup, and additional functionality added to BedToFasta.scala

--- src/main/scala/com/reljicd/BedToFasta.scala
+++ src/main/scala/com/reljicd/BedToFasta.scala
@@ -5,17 +5,15 @@ import org.apache.spark.sql.functions._
 
 case class Region(chromosome: String, start: Int, end: Int)
 
-case class RegionRows(region: Region, rowsRange: List[Int])
+case class RegionRowsList(region: Region, rowsList: List[Int])
 
-case class RegionRow(region: Region, row: BigInt)
+case class RegionRow(region: Region, row: Int)
 
-case class ReferenceFileRow(value: String, row: BigInt)
+case class ChromosomeNucleobasesRow(chromosome: String, nucleobases: String, row: Int)
 
-case class RegionReferenceRow(region: Region, value: String, row: BigInt)
+case class RegionNucleobases(region: Region, nucleobases: String)
 
-case class RegionReferenceRowValue(region: Region, value: String)
-
-case class RegionFasta(region: Region, value: String)
+case class RegionFasta(region: Region, fasta: String)
 
 object BedToFasta {
 
@@ -23,85 +21,76 @@ object BedToFasta {
 
   def main(args: Array[String]) {
     if (args.length < 3) {
-      System.err.println("Usage: BedToFasta <bedFile>  <referenceFile>  <outputFile> ")
+      System.err.println("Usage: BedToFasta <bedFile>  <referenceFiles directory>  <outputFile> ")
       System.exit(1)
     }
 
     val bedFile = args(0)
-    val referenceFile = args(1)
+    val referenceFilesDir = args(1)
     val outputFile = args(2)
 
     val sparkSession = SparkSession.builder.master("local").appName("Bed to Fasta").getOrCreate()
-    //    val sc = sparkSession.sparkContext
     import sparkSession.implicits._
 
-    //    val bedFileData = sc.textFile(bedFile)
-    //    val bedFileDataMap = bedFileData
-    //      .map(_.split("\t"))
-    //      .map(x => (x(0), x(1).toInt, x(2).toInt, x(3), x(4).toInt, x(5)))
-    val referenceFileDataSet = sparkSession.read.textFile(referenceFile)
-      .withColumn("row", monotonically_increasing_id)
-      .as[ReferenceFileRow]
-    //      .map(_.toList)
-    //      .flatMap(identity)
-
-    //    val regions = bedFileData.map(_.split("\t")).map(x => (x(0), x(1).toInt, x(2).toInt))
-
-
-    //    val bedFileDataMap1 = bedFileDataMap.map {
-    //      x => Bed(x(0), x(1).toInt, x(2).toInt, x(5))
-    //    }
-
-    //    val bedFileDataSet = bedFileDataMap1.toDF().as[Bed]
-
-    //        val bedFileDataset = sparkSession.read.format("tsv")
-    //          .option("sep", "\t")
-    //          .option("inferSchema", "true")
-    //          .textFile(bedFile).as[Bed]
-
+    // Map bed file to Region case class
     val regionsDataSet = sparkSession.read.textFile(bedFile)
       .map(_.split("\t"))
       .map(x => Region(chromosome = x(0), start = x(1).toInt, end = x(2).toInt))
-    //    val bedFileDataSet2 = sparkSession.read.textFile(bedFile).map(_.split("\t")).toDF("chromosome", "start", "end", "", "", "strand").as[Bed]
 
-    val regionsRowsDataSet = regionsDataSet
+    // Extract chromosomes from regionsDataSet
+    val chromosomes = regionsDataSet.map(x => x.chromosome).distinct().collect()
+
+    var chromosomeNucleobasesRowDataSet = sparkSession.emptyDataset[ChromosomeNucleobasesRow]
+
+    // Construct ChromosomeNucleobasesRow dataset from all the .fa files, by iterating through all the extracted chromosomes,
+    // and constructing reference files names from .fa files directory name and chromosome names
+    for (chromosome <- chromosomes) {
+      val referenceFile = "%s/%s.fa".format(referenceFilesDir, chromosome)
+      chromosomeNucleobasesRowDataSet = chromosomeNucleobasesRowDataSet.union(
+        sparkSession.read.textFile(referenceFile)
+          .withColumn("row", monotonically_increasing_id)
+          .map(x => ChromosomeNucleobasesRow(chromosome = chromosome, nucleobases = x.getString(0), row = x.getLong(1).toInt)))
+    }
+
+    // Construct dataset of regions and row numbers lists (RegionRowsList), based on referenceCharsPerRow
+    val regionsRowsListDataSet = regionsDataSet
       .map(region =>
-        RegionRows(
+        RegionRowsList(
           region = region,
-          rowsRange = Math.floor(region.start.toDouble / referenceCharsPerRow).toInt to
+          rowsList = Math.floor(region.start.toDouble / referenceCharsPerRow).toInt to
             Math.ceil(region.end.toDouble / referenceCharsPerRow).toInt toList))
-    //          rowStart = Math.floor(region.start.toDouble / referenceCharsPerRow).toInt,
-    //          rowEnd = Math.ceil(region.end.toDouble / referenceCharsPerRow).toInt))
 
-    //    sc.stop()
-
-    val regionRowDataSet = regionsRowsDataSet
-      .flatMap(regionRows => {
+    // Construct dataset of regions and row numbers by flattening all the rows lists
+    val regionRowDataSet = regionsRowsListDataSet
+      .flatMap(RegionRowsList => {
         val regionRowList = scala.collection.mutable.MutableList[RegionRow]()
-        regionRows.rowsRange.foreach(
-          regionRowList += RegionRow(regionRows.region, _)
+        RegionRowsList.rowsList.foreach(
+          regionRowList += RegionRow(RegionRowsList.region, _)
         )
         regionRowList
       })
 
-    val regionReferenceRowDataSet = regionRowDataSet
-      .joinWith(referenceFileDataSet, regionRowDataSet("row") === referenceFileDataSet("row"))
-      //      .as[RegionRowReferenceFileRow]
-      .map(x => RegionReferenceRow(region = x._1.region, value = x._2.value, row = x._2.row))
-    //        .groupByKey(x => x._1)
-    //      .reduceGroups((x, y) => (x._1, x._2 + y._2))
-
-    val regionReferenceRowsListDataSet = regionReferenceRowDataSet.groupByKey(_.region)
-      .reduceGroups((x, y) => RegionReferenceRow(region = x.region, value = x.value + y.value, row = 0))
-      .map(x => RegionReferenceRowValue(region = x._1, value = x._2.value))
+    // Join ChromosomeNucleobasesRow dataset and RegionRow dataset on "row" and "chromosome",
+    // group them by regions, then reduce for each region by nucleobases concatenation,
+    // and then return RegionNucleobase dataset ordered by "region"
+    val RegionNucleobasesDataSet = regionRowDataSet
+      .joinWith(chromosomeNucleobasesRowDataSet,
+        regionRowDataSet("row") === chromosomeNucleobasesRowDataSet("row")
+          && regionRowDataSet("region.chromosome") === chromosomeNucleobasesRowDataSet("chromosome"))
+      .map(x => RegionNucleobases(region = x._1.region, nucleobases = x._2.nucleobases))
+      .groupByKey(_.region)
+      .reduceGroups((x, y) => RegionNucleobases(region = x.region, nucleobases = x.nucleobases + y.nucleobases))
+      .map(x => x._2)
       .orderBy("region")
 
-    val regionFastaDataSet = regionReferenceRowsListDataSet
+
+    // Remove nucleobases not in the region, and return RegionFasta dataset
+    val regionFastaDataSet = RegionNucleobasesDataSet
       .map(x => {
-        val stringLength = x.value.length
-        val start = x.region.start%referenceCharsPerRow
-        val end = stringLength - (referenceCharsPerRow - x.region.end%referenceCharsPerRow)
-        RegionFasta(region = x.region, value = x.value.slice(start, end))
+        val stringLength = x.nucleobases.length
+        val start = x.region.start % referenceCharsPerRow
+        val end = stringLength - (referenceCharsPerRow - x.region.end % referenceCharsPerRow)
+        RegionFasta(region = x.region, fasta = x.nucleobases.slice(start, end))
       }
       )
 
