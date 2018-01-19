fb03edb6aeae5f4b8120ddb0a8f4fce037ef1ffa
Dušan Reljić <reljicd@gmail.com>
2018-01-19 10:57:13 +0100

    implemented integration test for BedToFasta

---
+++ src/test/scala/com/reljicd/BedToFastaTest.scala
@@ -0,0 +1,29 @@
+package com.reljicd
+
+import com.reljicd.BedToFasta.{calculateRegionFastaDS, readChromosomeNucleobasesRowDS, readRegionsDS}
+import com.holdenkarau.spark.testing.DatasetSuiteBase
+import org.scalatest.FunSuite
+
+class BedToFastaTest extends FunSuite with DatasetSuiteBase {
+  val bedFile = "src/test/data/BedToFastaTest/bed_file.bed"
+  val referenceFilesDir = "src/test/data/BedToFastaTest"
+  val referenceFastaFile = "src/test/data/BedToFastaTest/reference_fastahack_file.fa"
+
+  test("integration test") {
+    import spark.implicits._
+
+    val chromosomeNucleobasesRowDS = readChromosomeNucleobasesRowDS(spark, referenceFilesDir)
+
+    val regionsDS = readRegionsDS(spark, List(bedFile))
+
+    val regionFastaDS = calculateRegionFastaDS(spark, regionsDS, chromosomeNucleobasesRowDS)
+
+    val calculatedFastaDS = regionFastaDS.map(x => x.fasta)
+
+    val referenceFastaDS = spark.read.textFile(referenceFastaFile)
+      .map(x => x.toLowerCase)
+
+    assertDatasetEquals(referenceFastaDS, calculatedFastaDS)
+
+  }
+}
